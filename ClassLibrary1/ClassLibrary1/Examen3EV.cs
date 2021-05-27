26ee091ade63ff2fdee02c8e64a9972797819234
Jose Ramón García Sevilla <jrgarcia@iesmarenostrum.com>
2021-05-27 10:40:30 +0200

    Versión para inicio del examen de la tercera evaluación. Contiene dos proyectos: un proyecto de prueba, semi preparado, y un código para corregir (2EV), refactorizar (2-3EV), documentar (3EV) y probar (2-3EV).

---
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Examen3EV_NS
+{
+    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    {
+        public int sus;  // Suspensos
+        public int apr;  // Aprobados
+        public int not;  // Notables
+        public int sbr;  // Sobresalientes
+
+        public double media; // Nota media
+
+        // Constructor vacío
+        public estNot()
+        {
+            sus = apr = not = sbr = 0;  // inicializamos las variables
+            media = 0.0;
+        }
+
+        // Constructor a partir de un array, calcula las estadísticas al crear el objeto
+        public estNot(List<int> listnot)
+        {
+            media = 0.0;
+
+            for (int i = 0; i < listnot.Count; i++)
+            {
+                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
+                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
+                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
+                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+
+                media = media + listnot[i];
+            }
+
+            media = media / listnot.Count;
+        }
+
+
+        // Para un conjunto de listnot, calculamos las estadísticas
+        // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
+        //
+        // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
+        public double calcEst(List<int> listnot)
+        {                                 
+            media = 0.0;
+
+            // TODO: hay que modificar el tratamiento de errores para generar excepciones
+            //
+            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
+                return -1;
+
+            for (int i=0;i<10;i++)
+                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
+                return -1;
+
+            for (int i = 0; i < listnot.Count; i++)
+            {
+                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
+                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
+                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
+                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+
+                media = media + listnot[i];
+            }
+
+            media = media / listnot.Count;
+
+            return media;
+        }
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Examen3EV_NS;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> notas = new List<int>();
+
+            notas.Add(0);
+            notas.Add(5);
+            notas.Add(9);
+            notas.Add(3);
+            notas.Add(7);
+            notas.Add(4);
+            notas.Add(8);
+
+            double mediaEsperada = 5.143;
+            int susE = 3;
+            int aprE = 1;
+            int notE = 2;
+            int sbrE = 1;
+        }
+    }
+}
