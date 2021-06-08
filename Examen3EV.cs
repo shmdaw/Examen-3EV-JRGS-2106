c25221813160f15560eef407031cd5570e7ab67a
Sergio Hernandez Miguel <61750326+shmdaw@users.noreply.github.com>
2021-06-08 20:22:19 +0200

    Entrega shm2021
    No me ha dejado usar ni modo comando ni vs, he limpiado todo y modificado git config.

---
+++ Examen3EV.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System;
+namespace Examen3EV_NS_shm2021
+{/// <summary>
+/// <para>Clase estadisticaNotas</para>
+/// </summary>
+    public class EstadisticaNotas  // esta clase nos calcula las estadísticas de un conjunto de notas shm2021 
+    {
+        private int suspensos;  // Suspensos
+        private int aprobados;  // Aprobados
+        private int notables;  // Notables
+        private int sobresalientes;  // Sobresalientes
+
+        private double media; // Nota media
+
+
+        /// <summary>
+        /// <para>Constructor de estadisticaNotas</para>
+        /// </summary>
+        /// <param name="suspensos"></param>
+        /// <param name="aprobados"></param>
+        /// <param name="notables"></param>
+        /// <param name="sobresalientes"></param>
+        /// <param name="media"></param>
+        public void setNotas(int suspensos, int aprobados, int notables, int sobresalientes, double media)
+        {
+            try
+            {
+                this.suspensos = suspensos;
+                this.aprobados = aprobados;
+                this.notables = notables;
+                this.sobresalientes = sobresalientes;
+                this.media = media;
+            }
+            catch(Exception e)
+            {
+
+            }
+
+        }
+
+        /// <summary>
+        /// <para>Cosntructor vacio para estadisticaNotas, se inicializan las variables a 0</para>
+        /// </summary>
+        public EstadisticaNotas()
+        {
+            suspensos = aprobados = notables = sobresalientes = 0;  // inicializamos las variables
+            media = 0.0;
+        }
+
+        /// <summary>
+        /// <para>Constructor a partir de un array, calcula las estadísticas al crear el objeto</para>
+        /// </summary>
+        /// <param name="listaNotas"></param>
+        public EstadisticaNotas(List<int> listaNotas)
+        {
+            media = 0.0;
+
+            for (int i = 0; i < listaNotas.Count; i++)
+            {
+                if (listaNotas[i] < 5) suspensos++;              // Por debajo de 5 suspenso
+                else if (listaNotas[i] > 5 && listaNotas[i] < 7) aprobados++;// Nota para aprobar: 5
+                else if (listaNotas[i] > 7 && listaNotas[i] < 9) notables++;// Nota para notable: 7
+                else if (listaNotas[i] > 9) sobresalientes++;         // Nota para sobresaliente: 9
+
+                media = media + listaNotas[i];
+            }
+
+            media = media / listaNotas.Count;
+
+            setNotas(suspensos, aprobados, notables, sobresalientes, media);
+        }
+
+
+
+        /// <summary>
+        /// <para>Para un conjunto de listnot, calculamos las estadísticas</para>
+        /// <para>calcula la media y el número de suspensos/aprobados/notables/sobresalientes</para>
+        /// </summary>
+        /// <param name="listnot"></param>
+        /// <returns>Devuelve error si la lista no contiene elementos o si no están comprendidos entre 0 y 10</returns>
+        /// <returns>Devuelve la media</returns>
+        public double CalcEstadisticas(List<int> listnot)
+        {                                 
+            media = 0.0;
+
+            // TODO: hay que modificar el tratamiento de errores para generar excepciones
+            //
+            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
+                throw new ArgumentOutOfRangeException();
+
+            for (int i = 0; i < 10; i++)
+                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
+                    throw new Exception("Error, las notas deben comprenderse entre 0 y 10");
+
+            for (int i = 0; i < listnot.Count; i++)
+            {
+                if (listnot[i] < 5) suspensos++;              // Por debajo de 5 suspenso
+                else if (listnot[i] >= 5 && listnot[i] < 7) aprobados++;// Nota para aprobar: 5
+                else if (listnot[i] >= 7 && listnot[i] < 9) notables++;// Nota para notable: 7
+                else if (listnot[i] > 9) sobresalientes++;         // Nota para sobresaliente: 9
+
+                media = media + listnot[i];
+            }
+
+            media = media / listnot.Count;
+
+            return media;
+
+        }
+
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Examen3EV_NS_shm2021;
+using System;
+
+
+//Estoy teniendo problemas con VS, no me comprueba cosas y no me autocompleta.
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MediaEsperada()
+        {
+
+            //Preparar
+
+            List<int> notas = new List<int>();
+
+            notas.Add(0);
+            notas.Add(5);
+            notas.Add(9);
+            notas.Add(3);
+            notas.Add(7);
+            notas.Add(4);
+            notas.Add(8);
+
+            double mediaEsperada = 5.143;
+
+            //Probar
+
+            EstadisticaNotas pruebashm2021 = new EstadisticaNotas();
+            double media = pruebashm2021.CalcEstadisticas(notas);
+
+            //Assert
+
+            Assert.AreNotEqual(mediaEsperada,media,0.001,"La media no es la esperada");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+
+        public void ElementosMenoroIgualQueCero()
+        {
+            //Preparar
+
+            List<int> notas = new List<int>();
+
+            //Probar
+
+            EstadisticaNotas pruebaMenorQueCero = new EstadisticaNotas();
+            double media = pruebaMenorQueCero.CalcEstadisticas(notas);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void ElementosMenorQueCeroYMayorQueDiez()
+        {
+            //Preparar
+
+            List<int> notas = new List<int>();
+
+            //Probar
+
+            EstadisticaNotas pruebaMenorQueCero = new EstadisticaNotas();
+            double media = pruebaMenorQueCero.CalcEstadisticas(notas);
+
+        }
+
+        [TestMethod]
+        public void Pruebashm2021()
+        {
+            int susE = 3;
+            int aprE = 1;
+            int notE = 2;
+            int sbrE = 1;
+
+
+        }
+    }
+}
